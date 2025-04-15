6bf4b69cb85c8196b571bcba18bddc4813ee1cf8
milin_michael <milin.michael@kalkitech.com>
2025-04-15 17:44:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = [
+  "Choose your item 🧁",
+  "Pick the quantity 🔢",
+  "Enter the delivery address 📬",
+  "Submit your Order 🛒",
+];
+
+export default function App(){
+
+let [step, setStep] = useState(1);
+const[isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious(){
+    if(step>1)
+    setStep(step-1);
+  }
+
+  function handleNext(){
+    if(step<4)
+    setStep(step+1);
+  }
+
+return (
+<>
+  <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+  {
+    isOpen && (
+<div className="steps">
+  <div className="numbers">
+    <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+    <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+    <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+    <div className={`${step >= 3 ? "active" : ""}`}>4</div>
+  </div>
+
+  <p className="message"> Step: {step} : {messages[step-1]}</p>
+
+  <div className="buttons">
+    <button style={{backgroundColor:"#f792b4", color:"#ffff"}} 
+    onClick={handlePrevious}
+    >Previous</button>
+    <button style={{backgroundColor:"#f792b4", color:"#ffff"}} 
+    onClick={handleNext}
+    >Next</button>
+  </div>
+
+</div>
+)}
+</>
+
+);
+
 }
-
-export default App;
